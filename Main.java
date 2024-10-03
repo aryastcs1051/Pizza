4985455c18308a1a6868664e0b3aa26c7b6010dd
aryastcs1051 <aryas-farmington@thecoderschool.com>
2024-10-03 20:58:34 +0000

    Update Main File

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main{
     public static void main(String args[]) {
         System.out.println("Hello world");
+        PizzaShop shop = new PizzaShop ();
+        shop.placeOrder ();
     }
 }
